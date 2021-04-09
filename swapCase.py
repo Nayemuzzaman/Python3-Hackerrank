4c3e22443e94331a0f28e18cb16904c34aa4f39d
Nayemuzzaman <nayemzaman@gmail.com>
2021-04-09 23:50:23 +0600

    Hackerrank solution of python Swap Case

---
+++ swapCase.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/swap-case/problem
+
+def change(s):
+   if str.islower(s):
+       return str.upper(s)
+   else:
+       return str.lower(s)
+def swap_case(s):
+  return ("").join(map(change, s))
+
+if __name__ == '__main__':
+   s = input()
+   result = swap_case(s)
+   print(result)
